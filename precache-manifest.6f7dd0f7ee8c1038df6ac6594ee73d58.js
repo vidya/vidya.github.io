5cd68e0a3e641aebed3e6bbdb0d907513023d542
vidya <vsagarj@gmail.com>
2020-06-15 11:34:11 -0700

    Updates

---
+++ precache-manifest.6f7dd0f7ee8c1038df6ac6594ee73d58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a797daa799a660d128d8a6758137d94",
+    "url": "/heroku-react-test-app/index.html"
+  },
+  {
+    "revision": "f059297b6b9fdbd5b8f7",
+    "url": "/heroku-react-test-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb5a402016b0f232ebc9",
+    "url": "/heroku-react-test-app/static/js/2.665211f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/heroku-react-test-app/static/js/2.665211f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f059297b6b9fdbd5b8f7",
+    "url": "/heroku-react-test-app/static/js/main.f9952f7c.chunk.js"
+  },
+  {
+    "revision": "b519d4ff0fb4e04be0a6",
+    "url": "/heroku-react-test-app/static/js/runtime-main.0e224554.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/heroku-react-test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
